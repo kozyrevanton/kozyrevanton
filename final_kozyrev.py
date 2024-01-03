816938bad931e8131c509dde196316f6e6da2f5b
kozyrevanton <155246496+kozyrevanton@users.noreply.github.com>
2024-01-04 01:20:39 +0700

    Update final_kozyrev.py

--- final_kozyrev.py
+++ final_kozyrev.py
@@ -517,9 +517,8 @@ def Learning_model(KNN_check=True,LR_check=True,AB_check=True,RF_check=True,XGB_
         
         print("CB модель обучена")
     return best_Model
-def main():
+def main(model):
     page = st.sidebar.selectbox("Выбрать страницу", ["Параметры моделей и выбор лучшей модели", "Выполнение прогноза банкротства"])
-    model = 0
     if page == "Параметры моделей и выбор лучшей модели":
         st.header("""Параметры для обучения моделей. Будет выбрана лучшая модель по метрике F-1, из всех ниже перечисленных, по этим параметрам:""")
         st.write(model)
@@ -561,4 +560,5 @@ def predict_bunkrot(model,file_data):
     st.write(y)
    
 if __name__ == "__main__":
-    main()
+    model = 0
+    main(model)
